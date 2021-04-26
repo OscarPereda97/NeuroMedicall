71e6c89df8c58954da0c7f15bfeddcca9d61cbae
Oscar Pereda <oscarmpereda@hotmail.com>
2021-04-26 02:39:06 -0500

    mismos cambios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react"
 import LoginForm from '../components/LoginForm'
 import 'bootstrap/dist/css/bootstrap.css'
 import './css/Login.css'
+import logo from '../images/Logo.png'
 
 const Login = () => {
     return (
@@ -14,6 +15,7 @@ const Login = () => {
                         <div className="col-md-5 col-12 ">
                             <div className="row justify-content-center">
                                 <div className="col-md-8 col-sm-8 col-10">
+                                    <img src={logo} alt="" className="img-fluid px-5 my-4"/>
                                     <LoginForm />
                                 </div>
                             </div>
