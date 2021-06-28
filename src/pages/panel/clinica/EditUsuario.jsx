925a8d4e223c46dc719b0cb78b0077fbc8792b06
Oscar Pereda <oscarmpereda@hotmail.com>
2021-06-28 09:29:56 -0500

    make it better

--- src/pages/panel/clinica/EditUsuario.jsx
+++ src/pages/panel/clinica/EditUsuario.jsx
@@ -3,7 +3,7 @@ import { Link , useParams} from 'react-router-dom';
 
 const AddUsuario = () => {
     const {id} = useParams();
-    const urlBase = 'http://localhost:8000'
+    const urlBase = 'https://neuromedicall-backend.herokuapp.com'
 
     const [user, setUser] = useState({})
     const [password, setPassword]  = useState({})
